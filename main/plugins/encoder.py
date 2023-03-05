e510402bcad152d471d1fb6b05e6865f54734912
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-03-05 08:48:25 +0530

    Update encoder.py

--- main/plugins/encoder.py
+++ main/plugins/encoder.py
@@ -12,8 +12,8 @@ from ethon.pyfunc import video_metadata
 from .. import Star_Bots_Tamil, BOT_UN, LOG_CHANNEL
 
 from main.plugins.actions import LOG_START, LOG_END
-from LOCAL.localisation import SUPPORT_LINK, JPG, JPG2, JPG3
-from LOCAL.utils import ffmpeg_progress
+from Star-Bots-Tamil.localisation import SUPPORT_LINK, JPG, JPG2, JPG3
+from Star-Bots-Tamil.utils import ffmpeg_progress
 
 async def encode(event, msg, scale=0):
     ps_name = str(f"**{scale}p ENCODING:**")
