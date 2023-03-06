135c8ec82d162e6c24040dde0c183ee10a5b936a
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-03-06 06:52:16 +0530

    Update rename.py

--- main/plugins/rename.py
+++ main/plugins/rename.py
@@ -5,7 +5,7 @@ import os, time, requests
 from datetime import datetime as dt
 from telethon import events
 from telethon.tl.types import DocumentAttributeVideo
-from ethon.FastTelethonhelper import fast_download, fast_upload
+from ethon.fasttelethonhelper import fast_download, fast_upload
 from ethon.pyutils import rename
 from ethon.pyfunc import video_metadata
 
