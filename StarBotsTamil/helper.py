31d7836ad493fc8c26bd69c89c15494efae6a571
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-03-08 06:36:10 +0530

    Create helper.py

---
+++ StarBotsTamil/helper.py
@@ -0,0 +1,35 @@
+# (c) Star Bots Tamil
+
+    from decouple import config
+
+
+try:
+    from aiohttp import ClientSession as aiohttp_client
+except ImportError:
+    aiohttp_client = None
+    try:
+        import requests
+    except ImportError:
+        requests = None
+
+
+async def fast_download(download_url, filename=None, progress_callback=None):
+    if not aiohttp_client:
+        return await download_file(download_url, filename)[0], None
+    async with aiohttp_client() as session:
+        async with session.get(download_url, timeout=None) as response:
+            if not filename:
+                filename = unquote(download_url.rpartition("/")[-1])
+            total_size = int(response.headers.get("content-length", 0)) or None
+            downloaded_size = 0
+            start_time = time.time()
+            with open(filename, "wb") as f:
+                async for chunk in response.content.iter_chunked(1024):
+                    if chunk:
+                        f.write(chunk)
+                        downloaded_size += len(chunk)
+                    if progress_callback and total_size:
+                        await _maybe_await(
+                            progress_callback(downloaded_size, total_size)
+                        )
+            return filename, time.time() - start_time
