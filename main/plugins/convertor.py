68858d164f678978528b6a6b5e252c3dea53cf04
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-03-08 06:47:31 +0530

    Update convertor.py

--- main/plugins/convertor.py
+++ main/plugins/convertor.py
@@ -6,7 +6,7 @@ from datetime import datetime as dt
 from telethon import events
 from telethon.tl.types import DocumentAttributeVideo
 from ethon.telefunc import fast_upload
-from StarBotsTamil.helper fast_download
+from main.plugins.helper fast_download
 from ethon.pyfunc import bash, video_metadata
 from ethon.pyutils import rename
 
