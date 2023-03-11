0f043a5f9c24574a907bbf9f6bb8b7145c73673e
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-03-11 19:17:15 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -11,13 +11,13 @@ from StarBotsTamil.presents import spam_notice, help_text, SUPPORT_LINK, source_
 @Star_Bots_Tamil.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
     mention = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
-    await event.reply(f'Hi 👋🏻 {mention} ❤️,\nI am Star Bots Official Video Editor Bot.\nMaintenance By :- [Karthik](https://t.me/TG_Karthik)\n\nI Can do Multiple Things!!\n\nJust Send Me the File 📂 to Start with Me.\n\nYou Can Check in the Menu Our "🤖 Bot Features".', link_preview=False,
+    await event.reply(f'Hi 👋🏻 {mention} ❤️,\n\nI am Star Bots Official Video Editor Bot.\nMaintenance By :- [Karthik](https://t.me/TG_Karthik)\n\nI Can do Multiple Things!!\n\nJust Send Me the File 📂 to Start with Me.\n\nYou Can Check in the Menu Our "🤖 Bot Features".', link_preview=False,
                       buttons=[
                               [Button.inline("Bot's Menu", data="menu")],
                               [Button.url("🤖 Bot Channel", url="https://t.me/Star_Bots_Tamil")]
                               ])
     mention = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
-    await Star_Bots_Tamil.send_message(int(ACCESS_CHANNEL), f'**{mention} Started The Bot**')
+    await Star_Bots_Tamil.send_message(int(ACCESS_CHANNEL), f'{mention} Started The Bot')
     
 @Star_Bots_Tamil.on(events.callbackquery.CallbackQuery(data="menu"))
 async def menu(event):
