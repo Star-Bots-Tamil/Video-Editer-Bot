421fbb5036ce8dfceb104f1b085fbc544ad22c57
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-03-11 15:02:25 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -11,7 +11,7 @@ from StarBotsTamil.presents import spam_notice, help_text, SUPPORT_LINK, source_
 @Star_Bots_Tamil.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
     mention = event.from_user.mention
-    await event.reply(f'**Hi 👋🏻 {mention} ❤️,\nI am Star Bot's Official Video Editor Bot.\nMaintenance By :- [Karthik](https://t.me/TG_Karthik)\n\nI Can do Multiple Things!!\n\nJust Send Me the File 📂 to Start with Me.\n\nYou Can Check in the Menu My Actions.**', 
+    await event.reply(f'{start}', 
                       buttons=[
                               [Button.inline("Bot's Menu", data="menu")]
                               ])
@@ -48,6 +48,7 @@ async def help(event):
     
 @Star_Bots_Tamil.on(events.callbackquery.CallbackQuery(data="actions"))
 async def plugins(event):
+    mention = event.from_user.mention
     await event.edit(f'{help_text}',
                     buttons=[[Button.inline("Bot's Menu", data="menu")]])
                    
